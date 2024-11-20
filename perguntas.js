60b8f8da27730b0fee6fe4b58e0d25c381a930c0
rafissq <148448594+rafissq@users.noreply.github.com>
2024-11-20 11:49:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Posição',
+    'Líbero pode atacar?',
+    'Não. Sacar ou bloquear também não.'
+)
+
+criaCartao(
+    'Rotação',
+    'Qual sentido a rotação gira?',
+    'Sentido horário.'
+)
+
+criaCartao(
+    'Regras',
+    'O que é considerado invasão?',
+    'Quando sua mão passa pela rede e interfere uma ação adversária.'
+)
+
+criaCartao(
+    'Desafio',
+    'Quais são os 6 principais fundamentos do vôlei? ',
+    'Saque, passe, levantamento, ataque, bloqueio e defesa'
+)
